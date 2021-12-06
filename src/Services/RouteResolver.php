e34fd241ed9d84a51f08c3e16ce592fe252b4f35
Ramon Egger <ramon@ramonegger.ch>
2021-12-06 11:35:54 +0100

    expanded layout

---
+++ src/Services/RouteResolver.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+use App\Interfaces\ResolveRouteInterface;
+
+class RouteResolver implements ResolveRouteInterface
+{
+    public function __construct()
+    {
+        //
+    }
+}
