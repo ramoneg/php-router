22c9d044ed461b7b4fb02e75feccac58f43578ec
Ramon Egger <ramon@ramonegger.ch>
2021-12-06 17:40:46 +0100

    implemented route resolver functionality

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
 <?php
 
+use App\Models\Router;
+use App\Services\RouteResolver;
+
 require __DIR__ . '/../vendor/autoload.php';
+
+$routes = include(__DIR__ . '/../routes.php');
+
+$resolver = (new RouteResolver());
+$resolver->setRoutes($routes);
+
+$route = $_SERVER['REQUEST_URI'];
+
+$output = (new Router($resolver, $route))->resolve();
+
+print json_encode($output);
